3a064b06906e4a375ba0577adef009f45f7a9100
gokulkrishnancm <gokulkrishnan949@gmail.com>
2021-07-19 17:17:06 +0530

    Improved file.h

--- file.h
+++ file.h
@@ -3,22 +3,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include <assert.h>
+
+#include "basic.h"
 
 typedef struct File File;
 struct File {
     const char *name;
-    FILE *fp;
-    size_t size;
-    bool is_closed;
+    FILE    *fp;
+    u64     size;
+    bool    is_closed;
 };
 
-File    file_init(const char *file_path);
-bool    file_open(File *file, const char *mode);
-void    file_destroy(File *file);
+File            file_init(const char *file_path);
+void            file_read_to_buf(File *file, char *buffer, size_t bytes);
+bool            file_open(File *file, const char *mode);
+char *          file_readall(File *file);
+void            file_destroy(File *file);
 
-size_t _file_get_size(const char *file_path)
+#define file_close(pfile) {\
+    fclose((*pfile).fp);\
+    (*pfile).is_closed = true;\
+}
+
+
+size_t file_get_size(const char *file_path)
 {
     FILE *fp = fopen(file_path, "r");
     if (fp == NULL) {
@@ -36,7 +47,7 @@ File file_init(const char *file_path)
 {
     File file = {0};
     file.name = file_path;
-    file.size = _file_get_size(file_path);
+    file.size = file_get_size(file_path);
     file.fp = NULL;
     file.is_closed = true;
 
@@ -53,13 +64,47 @@ bool file_open(File *file, const char *mode)
     return true;
 }
 
+
+void file_read_to_buf(File *file, char *buffer, size_t bytes)
+{
+    assert(file != NULL);
+    assert(buffer != NULL);
+    assert(bytes <= file->size);
+
+    if (file->is_closed || file->fp == NULL) 
+        if(!file_open(file, "r")) 
+            eprint("Error: unable to open file");
+    
+
+    fread(buffer, bytes, 1, file->fp);
+    file_destroy(file);
+}
+
+char * file_readall(File *file)
+{
+    //NOTE: this code works dont tinker
+    
+    assert(file);
+
+    char *buffer = (char *)malloc(file->size+1);
+    if (buffer == NULL) {
+        fprintf(stderr, "%s: malloc failed\n", __func__);
+        exit(1);
+    }
+
+    file_read_to_buf(file, buffer, file->size);
+    buffer[file->size] = '\0';
+
+    return buffer;
+}
+
 void file_destroy(File *file)
 {
-    if (file->is_closed == false) {
-        fclose(file->fp);
-        file->fp = NULL;
-        file->is_closed = true;
-    }         
+    assert(file != NULL);
+    if (!file->is_closed) fclose(file->fp);
+
+    file->fp = NULL;
+    file->is_closed = true;
 }
 
 
