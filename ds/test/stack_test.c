08457bcd71606b778bfacd7bf22f32f445c6a6af
gimploo <gokulkrishnan949@gmail.com>
2021-08-20 15:13:42 +0530

    stack_test

--- ds/test/stack_test.c
+++ ds/test/stack_test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "../stack.h"
+#include "../../math/la.h"
 
 
 void print_int(void *x)
@@ -26,44 +27,79 @@ void print_foo(void *x)
     printf("%li ",foo->size);
 }
 
-int main(void)
+void print_vec4f(void *x)
 {
-    u64 foo[10];
-    stack_t stack01 = stack_init(foo, 10);
-
-    stack_print(&stack01, print_int);
-
-    for (u64 i = 1; i <= 10; i++) stack_push(&stack01, (void *)i);
-
-    stack_print(&stack01, print_int);
-
-    stack_dump(&stack01);
-
-    return 0;
+    vec4f_t *vec = (vec4f_t *)x;
+    printf(VEC4F_FMT"\n", VEC4F_ARG(vec));
 }
-
 
 int oldmain(void)
 {
-    int **array = (int **) malloc(sizeof(int *) * 5);
-    stack_t stack = stack_init(array, 5); 
+    int foo[10];
+
+    stack_t stack = stack_init(foo, 10);
 
     stack_dump(&stack);
-
-    stack_push(&stack ,1);
+    for (int i = 1; i <= 10; i++)
+        stack_push(&stack, i);
     stack_dump(&stack);
-    stack_delete(&stack);
-    stack_dump(&stack);
-    stack_push(&stack ,2);
-    stack_push(&stack ,3);
-    stack_push(&stack ,4);
-    stack_pop(&stack);
-    stack_push(&stack ,5);
 
     stack_print(&stack, print_int);
 
-    stack_dump(&stack);
-    
-    free(array);
+}
+
+
+int main(void)
+{
+    void ** foo[10] = {0};
+
+    vec4f_t vecs[10] = {0}; 
+
+    stack_t stack01 = stack_init(foo, 10);
+    stack_t stack02 = stack_init(vecs, 10);
+    stack_dump(&stack01);
+
+    vec4f_t data[10] = {
+
+        vec4f(0.0f),
+        vec4f(1.0f),
+        vec4f(2.0f),
+        vec4f(3.0f),
+        vec4f(4.0f),
+        vec4f(5.0f),
+        vec4f(6.0f),
+        vec4f(7.0f),
+        vec4f(8.0f),
+        vec4f(9.0f),
+    }; 
+
+    for (int i = 0; i < 10; i++)
+    {
+        /*stack_push(&stack01, &data[i], sizeof(data[i]));*/
+        stack_push(&stack02, data[i]);
+    }
+    /*stack_print(&stack01, print_vec4f);*/
+    printf("\n");
+
+    /*for_i_in_stack(&stack01)*/
+    {
+        /*vec4f_t *vec = ((vec4f_t *)stack01.array + i);*/
+        /*printf(VEC4F_FMT"\n", VEC4F_ARG(vec));*/
+    }
+    /*printf("\n");*/
+
+    for_i_in_stack(&stack02)
+    {
+        vec4f_t *vec = ((vec4f_t *)stack02.array + i);
+        printf(VEC4F_FMT"\n", VEC4F_ARG(vec));
+    }
+    printf("\n");
+
+    /*for (int i = 0; i < 10; i++)*/
+        /*printf(VEC4F_FMT"\n", VEC4F_ARG(&vecs[i]));*/
+
+
     return 0;
 }
+
+
