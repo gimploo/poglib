a2490ff3828b7e2292fc12cbb9d71d3b6764be6a
gimploo <gokulkrishnan949@gmail.com>
2021-08-20 22:41:41 +0530

    Major refactoring: changed up the style and foramtting

--- simple/gl/shader.h
+++ simple/gl/shader.h
@@ -5,16 +5,12 @@
  // OpenGL shader handling library
 =======================================================*/
 
+#include <string.h>
 #include "_common.h"
+#include "../../generics.h"
 
-typedef struct gl_shader_t {
-
-    GLuint id;
-    const char *vs_file_path;
-    const char *fg_file_path;
-
-} gl_shader_t;
-
+#define MAX_UNIFORM_ARRAY_CAPACITY 10
+#define MAX_UNIFORM_VALUE_SIZE (4 * sizeof(f32))
 
 const char * const default_vshader = 
     "#version 330 core\n"
@@ -43,181 +39,127 @@ const char * const default_fshader =
     "\n"
     "void main()\n"
     "{\n"
-        "FragColor = texture(u_texture01, tex_coord) * vec4(color, 1.0f);\n"
+        "FragColor = texture(u_texture01, tex_coord) + vec4(color, 1.0f);\n"
     "}";
 
-    
+
+typedef struct gl_shader_t {
+
+    GLuint      id;
+    const char  *vs_file_path;
+    const char  *fg_file_path;
+    stack_t     uniforms;
+
+} gl_shader_t;
+
+
+typedef void (*shader_uniform_set_arr_f) (const gl_shader_t *, const char *, void **);
+typedef void (*shader_uniform_set_val_f) (const gl_shader_t *, const char *, void *);
+
+typedef enum gl_shader_uniform_type {
+
+    UT_UINT = 0,
+    UT_INT,
+    UT_FLOAT,
+    UT_FLOAT_ARRAY,
+    UT_COUNT
+
+} gl_uniform_type;
+
+typedef struct __uniform_meta_data_t {
+
+    const char                      *variable_name;
+    const u64                       data_size;
+    const gl_uniform_type           uniform_type;
+    u8                              data_buffer[MAX_UNIFORM_VALUE_SIZE];
+
+} __uniform_meta_data_t;
 
 
 /*-----------------------------------------------------
  // Declarations
 -----------------------------------------------------*/
 
-//NOTE:(macro)  shader_default_init(void) --> gl_shader_t
-gl_shader_t     shader_init(const char *file_vs, const char *file_fs);
-gl_shader_t     shader_load_code(const char *vertex_source_code, const char *fragment_source_code);
+//NOTE:(macro)  gl_shader_default_init(void) --> gl_shader_t
 
-void            shader_bind(const gl_shader_t *shader);
+gl_shader_t     gl_shader_from_file_init(const char *file_vs, const char *file_fs);
+gl_shader_t     gl_shader_from_cstr_init(const char *vs_code, const char *fs_code);
 
-void            shader_set_farr(gl_shader_t *shader, const char *uniform, float arr[]);
-void            shader_set_fval(gl_shader_t *shader, const char *uniform, float val);
-void            shader_set_uival(gl_shader_t *shader, const char *uniform, unsigned int val);
-void            shader_set_ival(gl_shader_t *shader, const char *uniform, int val);
+void            gl_shader_bind(gl_shader_t *shader);
 
-void            shader_destroy(const gl_shader_t *shader);
+void            gl_shader_uniform_set_farr(gl_shader_t *shader, const char *uniform, float arr[]);
+void            gl_shader_uniform_set_fval(gl_shader_t *shader, const char *uniform, float val);
+void            gl_shader_uniform_set_uival(gl_shader_t *shader, const char *uniform, unsigned int val);
+void            gl_shader_uniform_set_ival(gl_shader_t *shader, const char *uniform, int val);
+
+void            gl_shader_push_uniform(
+                            gl_shader_t *shader, 
+                            const char *uniform_name, 
+                            void *value, 
+                            u64 value_size, 
+                            gl_uniform_type type);
+
+void            gl_shader_destroy(const gl_shader_t *shader);
 
 
 /*--------------------------------------------------------
  // Implementation
 --------------------------------------------------------*/
 
-#define shader_default_init() shader_load_code(default_vshader, default_fshader)
+#define gl_shader_default_init() gl_shader_from_cstr_init(default_vshader, default_fshader)
 
-void shader_set_ival(gl_shader_t *shader, const char *uniform, int val)
+static inline void __shader_load_code(gl_shader_t *shader, const char *vs_code, const char *fs_code)
 {
     if (shader == NULL) eprint("shader argument is null");
-    if (uniform == NULL) eprint("uniform argument is null");
 
-    shader_bind(shader);
-    int location;
-    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
-    if (location == -1) eprint("[ERROR] uniform doesnt exist");
-
-    GL_CHECK(glUniform1i(location, val));
-}
-
-void shader_set_uival(gl_shader_t *shader, const char *uniform, unsigned int val)
-{
-    if (shader == NULL) eprint("shader argument is null");
-    if (uniform == NULL) eprint("uniform argument is null");
-
-    shader_bind(shader);
-    int location;
-    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
-    if (location == -1) eprint("[ERROR] uniform doesnt exist");
-    GL_CHECK(glUniform1ui(location, val));
-}
-
-void shader_set_fval(gl_shader_t *shader, const char *uniform, float val)
-{
-    if (shader == NULL) eprint("shader argument is null");
-    if (uniform == NULL) eprint("uniform argument is null");
-
-    shader_bind(shader);
-    int location;
-    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
-    if (location == -1) eprint("[ERROR] uniform doesnt exist");
-    GL_CHECK(glUniform1f(location, val));
-}
-
-void shader_set_farr(gl_shader_t *shader, const char *uniform, float arr[])
-{
-    if (shader == NULL) eprint("shader argument is null");
-    if (uniform == NULL) eprint("uniform argument is null");
-
-    shader_bind(shader);
-
-    int location;
-    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
-    if (location == -1) eprint("[ERROR] uniform doesnt exist");
-    GL_CHECK(glUniform4f(location, arr[0], arr[1], arr[2], arr[3]));
-}
-
-
-
-inline void shader_destroy(const gl_shader_t *shader)
-{
-    if (shader == NULL) {
-        fprintf(stderr, "%s: shader is null\n", __func__);
-        exit(1);
-    }
-
-
-    GLuint id = shader->id;
-    GL_CHECK(glDeleteProgram(shader->id));
-    GL_LOG("gl_shader_t `%i` successfully deleted", id);
-}
-
-
-void shader_bind(const gl_shader_t *shader)
-{
-    if (shader == NULL) {
-        fprintf(stderr, "%s: shader is null\n", __func__);
-        exit(1);
-    }
-    GL_CHECK(glUseProgram(shader->id));
-}
-
-gl_shader_t shader_load_code(const char *vertex_source_code, const char *fragment_source_code)
-{
-    gl_shader_t shader = {0};
     int status;
     char error_log[KB] = {0};
 
-    const char * vtxfile = vertex_source_code;
-    if (vtxfile == NULL) {
-        fprintf(stderr, "%s: vertex file returned null\n", __func__);
-        exit(1);
-    }
-
-    const char *frgfile = fragment_source_code;
-    if (frgfile == NULL) {
-        fprintf(stderr, "%s: vertex file returned null\n", __func__);
-        exit(1);
-    }
-
     GLuint vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vtxfile, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    GL_CHECK(vertexShader = glCreateShader(GL_VERTEX_SHADER));
+    GL_CHECK(glShaderSource(vertexShader, 1, &vs_code, NULL));
+    GL_CHECK(glCompileShader(vertexShader));
+    GL_CHECK(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status));
     if (!status) {
-        glGetShaderInfoLog(vertexShader, KB, NULL, error_log);
-        fprintf(stderr, "Vertex Error:\n\t%s\n", error_log);
-        exit(1);
+        GL_CHECK(glGetShaderInfoLog(vertexShader, KB, NULL, error_log));
+        eprint("Vertex Error:\n\t%s\n", error_log);
     }
-   GL_LOG("Vertex Shader successfully compiled");
+    GL_LOG("Vertex Shader successfully compiled");
 
     GLuint fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &frgfile, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    GL_CHECK(fragmentShader = glCreateShader(GL_FRAGMENT_SHADER));
+    GL_CHECK(glShaderSource(fragmentShader, 1, &fs_code, NULL));
+    GL_CHECK(glCompileShader(fragmentShader));
+    GL_CHECK(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status));
     if (!status) {
-        glGetShaderInfoLog(fragmentShader, KB, NULL, error_log);
-        fprintf(stderr, "Fragment Error:\n\t%s\n", error_log);
-        exit(1);
+        GL_CHECK(glGetShaderInfoLog(fragmentShader, KB, NULL, error_log));
+        eprint("Fragment Error:\n\t%s\n", error_log);
     }
-   GL_LOG("Fragment Shader successfully compiled");
+    GL_LOG("Fragment Shader successfully compiled");
 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    GLuint shaderProgram; 
+    GL_CHECK(shaderProgram = glCreateProgram());
+    GL_CHECK(glAttachShader(shaderProgram, vertexShader));
+    GL_CHECK(glAttachShader(shaderProgram, fragmentShader));
+    GL_CHECK(glLinkProgram(shaderProgram));
+    GL_CHECK(glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status));
     if(!status) {
-        glGetProgramInfoLog(shaderProgram, KB, NULL, error_log);
-        fprintf(stderr, "Error: %s\n", error_log);
-        exit(1);
+        GL_CHECK(glGetProgramInfoLog(shaderProgram, KB, NULL, error_log));
+        eprint("Error: %s\n", error_log);
     }
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    GL_CHECK(glDeleteShader(vertexShader));
+    GL_CHECK(glDeleteShader(fragmentShader));
 
-    shader.id = shaderProgram;
+    shader->id = shaderProgram;
 
-   GL_LOG("Shader `%i` successfully linked", shader.id);
-
-    return shader;
+    GL_LOG("Shader `%i` successfully linked", shader->id);
 }
 
 
-
-gl_shader_t shader_init(const char *vertex_source_path, const char *fragment_source_path)
+static inline void __shader_load_from_file(gl_shader_t *shader, const char *vertex_source_path, const char *fragment_source_path)
 {
-    gl_shader_t shader = {0};
-    shader.vs_file_path = vertex_source_path;
-    shader.fg_file_path = fragment_source_path;
+    if (shader == NULL) eprint("shader argument is null");
 
     int status;
     char error_log[KB] = {0};
@@ -238,48 +180,211 @@ gl_shader_t shader_init(const char *vertex_source_path, const char *fragment_sou
     }
 
     GLuint vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vtxfile, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    GL_CHECK(vertexShader = glCreateShader(GL_VERTEX_SHADER));
+    GL_CHECK(glShaderSource(vertexShader, 1, &vtxfile, NULL));
+    GL_CHECK(glCompileShader(vertexShader));
+    GL_CHECK(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status));
     if (!status) {
         glGetShaderInfoLog(vertexShader, KB, NULL, error_log);
-        fprintf(stderr, "Vertex Error:\n\t%s\n", error_log);
-        exit(1);
+        eprint("Vertex Error:\n\t%s\n", error_log);
     }
-   GL_LOG("Vertex Shader successfully compiled");
+    GL_LOG("Vertex Shader successfully compiled");
 
     GLuint fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &frgfile, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    GL_CHECK(fragmentShader = glCreateShader(GL_FRAGMENT_SHADER));
+    GL_CHECK(glShaderSource(fragmentShader, 1, &frgfile, NULL));
+    GL_CHECK(glCompileShader(fragmentShader));
+    GL_CHECK(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status));
     if (!status) {
-        glGetShaderInfoLog(fragmentShader, KB, NULL, error_log);
-        fprintf(stderr, "Fragment Error:\n\t%s\n", error_log);
-        exit(1);
+        GL_CHECK(glGetShaderInfoLog(fragmentShader, KB, NULL, error_log));
+        eprint("Fragment Error:\n\t%s\n", error_log);
     }
-   GL_LOG("Fragment Shader successfully compiled");
+    GL_LOG("Fragment Shader successfully compiled");
 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    GLuint shaderProgram;
+    GL_CHECK(shaderProgram = glCreateProgram());
+    GL_CHECK(glAttachShader(shaderProgram, vertexShader));
+    GL_CHECK(glAttachShader(shaderProgram, fragmentShader));
+    GL_CHECK(glLinkProgram(shaderProgram));
+    GL_CHECK(glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status));
     if(!status) {
-        glGetProgramInfoLog(shaderProgram, KB, NULL, error_log);
-        fprintf(stderr, "Error: %s\n", error_log);
-        exit(1);
+        GL_CHECK(glGetProgramInfoLog(shaderProgram, KB, NULL, error_log));
+        eprint("Error: %s\n", error_log);
     }
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    GL_CHECK(glDeleteShader(vertexShader));
+    GL_CHECK(glDeleteShader(fragmentShader));
 
-    shader.id = shaderProgram;
+    shader->id = shaderProgram;
 
-   GL_LOG("Shader `%i` successfully linked", shader.id);
+   GL_LOG("Shader `%i` successfully linked", shader->id);
+}
+
+gl_shader_t  gl_shader_from_file_init(const char *file_vs, const char *file_fs)
+{
+    if (file_vs == NULL) eprint("file_vs argument is null");
+    if (file_fs == NULL) eprint("file_fs arguemnt is null");
+
+    gl_shader_t shader;
+    shader.fg_file_path = file_fs;
+    shader.vs_file_path = file_vs;
+
+    __shader_load_from_file(&shader, file_vs, file_fs);
+
+    __uniform_meta_data_t *array = (__uniform_meta_data_t *)malloc(MAX_UNIFORM_ARRAY_CAPACITY * sizeof(__uniform_meta_data_t));
+    if (array == NULL) eprint("malloc failed");
+
+    shader.uniforms = stack_init((void **)array, MAX_UNIFORM_ARRAY_CAPACITY);
 
     return shader;
+}
+
+gl_shader_t  gl_shader_from_cstr_init(const char *vs_code, const char *fs_code)
+{
+    if (vs_code == NULL) eprint("vs_code argument is null");
+    if (fs_code == NULL) eprint("fs_code arguemnt is null");
+
+    gl_shader_t shader;
+    shader.fg_file_path = NULL;
+    shader.vs_file_path = NULL;
+
+    __shader_load_code(&shader, vs_code, fs_code);
+
+    __uniform_meta_data_t *array = (__uniform_meta_data_t *)malloc(MAX_UNIFORM_ARRAY_CAPACITY * sizeof(__uniform_meta_data_t));
+    if (array == NULL) eprint("malloc failed");
+
+    shader.uniforms = stack_init((void **)array, MAX_UNIFORM_ARRAY_CAPACITY);
+
+    return shader;
+}
+
+
+void gl_shader_uniform_set_ival(gl_shader_t *shader, const char *uniform, int val)
+{
+    if (shader == NULL) eprint("shader argument is null");
+    if (uniform == NULL) eprint("uniform argument is null");
+
+    gl_shader_bind(shader);
+    int location;
+    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
+    if (location == -1) eprint("[ERROR] uniform doesnt exist");
+
+    GL_CHECK(glUniform1i(location, val));
+}
+
+void gl_shader_uniform_set_uival(gl_shader_t *shader, const char *uniform, unsigned int val)
+{
+    if (shader == NULL) eprint("shader argument is null");
+    if (uniform == NULL) eprint("uniform argument is null");
+
+    gl_shader_bind(shader);
+    int location;
+    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
+    if (location == -1) eprint("[ERROR] uniform doesnt exist");
+    GL_CHECK(glUniform1ui(location, val));
+}
+
+void gl_shader_uniform_set_fval(gl_shader_t *shader, const char *uniform, float val)
+{
+    if (shader == NULL) eprint("shader argument is null");
+    if (uniform == NULL) eprint("uniform argument is null");
+
+    gl_shader_bind(shader);
+    int location;
+    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
+    if (location == -1) eprint("[ERROR] uniform doesnt exist");
+    GL_CHECK(glUniform1f(location, val));
+}
+
+void gl_shader_uniform_set_farr(gl_shader_t *shader, const char *uniform, float arr[])
+{
+    if (shader == NULL) eprint("shader argument is null");
+    if (uniform == NULL) eprint("uniform argument is null");
+
+    gl_shader_bind(shader);
+
+    int location;
+    GL_CHECK(location = glGetUniformLocation(shader->id, uniform));
+    if (location == -1) eprint("[ERROR] uniform doesnt exist");
+    GL_CHECK(glUniform4f(location, arr[0], arr[1], arr[2], arr[3]));
+}
+
+
+
+inline void gl_shader_destroy(const gl_shader_t *shader)
+{
+    if (shader == NULL) eprint("shader argument is null");
+
+    free(shader->uniforms.array);
+
+    GL_CHECK(glDeleteProgram(shader->id));
+    GL_LOG("gl_shader_t `%i` successfully deleted", shader->id);
+}
+
+
+void gl_shader_bind(gl_shader_t *shader)
+{
+    if (shader == NULL) eprint("shader argument is null");
+
+    GL_CHECK(glUseProgram(shader->id));
+
+    for (int i = 0; i <= shader->uniforms.top; i++)
+    {
+        __uniform_meta_data_t *uniform = (__uniform_meta_data_t *)shader->uniforms.array + i;
+
+        switch(uniform->uniform_type)
+        {
+            case UT_UINT:
+                gl_shader_uniform_set_uival(shader, uniform->variable_name, *(u32 *)uniform->data_buffer);
+                break;
+            case UT_INT:
+                gl_shader_uniform_set_ival(shader, uniform->variable_name, *(i32 *)uniform->data_buffer);
+                break;
+            case UT_FLOAT:
+                gl_shader_uniform_set_fval(shader, uniform->variable_name, *(f32 *)uniform->data_buffer);
+                break;
+            case UT_FLOAT_ARRAY:
+                gl_shader_uniform_set_farr(shader, uniform->variable_name, (f32 *)uniform->data_buffer);
+                break;
+            default:
+                eprint("uniform type not accounted for");
+        }
+    }
+        
+}
+
+__uniform_meta_data_t __uniform_meta_data_init(const char *var_name, void *data_ref, const u64 data_size, const gl_uniform_type type)
+{
+    if (var_name == NULL) eprint("var_name argument is null");
+    if (data_ref == NULL) eprint("data_ref argument is null");
+
+    assert(data_size <= MAX_UNIFORM_VALUE_SIZE);
+
+    __uniform_meta_data_t data = {
+        .variable_name  = var_name,
+        .data_size      = data_size,
+        .uniform_type   = type,
+    };
+
+    memcpy(data.data_buffer, data_ref, data_size); 
+
+    return data;
+}
+
+
+void gl_shader_push_uniform(gl_shader_t *shader, const char *uniform_name, void *value, u64 value_size, gl_uniform_type type) 
+{
+    if (shader == NULL) eprint("shader arg is null");    
+    if (value == NULL) eprint("value arg is null");
+
+    __uniform_meta_data_t data = __uniform_meta_data_init(
+            uniform_name, 
+            value, 
+            value_size, 
+            type);
+
+    stack_push(&shader->uniforms, data);
+
 }
 
 
