fdc1556dd4f79de2b6576dd076f37f7fc721536d
gimploo <gokulkrishnan949@gmail.com>
2021-08-25 23:51:55 +0530

    Added more types

--- math/la.h
+++ math/la.h
@@ -4,7 +4,8 @@
 #include "../basic.h"
 #include <math.h>
 
-#define norm(x, min, max) (((x) - (min))/((max) - (min)))
+#define normalize(x, min, max)   (((x) - (min))/((max) - (min)))
+#define denormalize(x, min, max) ((x) * ((max) - (min)) + (min))
 
 #define X 0
 #define Y 1
@@ -25,6 +26,47 @@ struct vec4_t {
     u8 cmp[4];
 };
 
+typedef struct vec2ui_t vec2ui_t;
+typedef struct vec3ui_t vec3ui_t;
+typedef struct vec4ui_t vec4ui_t;
+
+struct vec2ui_t { 
+    u32 cmp[2];
+}; 
+struct vec3ui_t { 
+    u32 cmp[3];
+};
+struct vec4ui_t { 
+    u32 cmp[4];
+};
+#define VEC2UI_FMT       "(%i,%i)"
+#define VEC2UI_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1]
+#define VEC3UI_FMT       "(%i,%i,%i)"
+#define VEC3UI_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1], (pvec)->cmp[2]
+#define VEC4UI_FMT       "(%i,%i,%i,%i)"
+#define VEC4UI_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1], (pvec)->cmp[2], (pvec)->cmp[3]
+
+typedef struct vec2i_t vec2i_t;
+typedef struct vec3i_t vec3i_t;
+typedef struct vec4i_t vec4i_t;
+
+struct vec2i_t { 
+    i32 cmp[2];
+}; 
+struct vec3i_t { 
+    i32 cmp[3];
+};
+struct vec4i_t { 
+    i32 cmp[4];
+};
+
+#define VEC2I_FMT       "(%d,%d)"
+#define VEC2I_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1]
+#define VEC3I_FMT       "(%d,%d,%d)"
+#define VEC3I_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1], (pvec)->cmp[2]
+#define VEC4I_FMT       "(%d,%d,%d,%d)"
+#define VEC4I_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1], (pvec)->cmp[2], (pvec)->cmp[3]
+
 
 #define VEC2F_FMT       "(%f,%f)"
 #define VEC2F_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1]
@@ -32,6 +74,7 @@ struct vec4_t {
 #define VEC3F_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1], (pvec)->cmp[2]
 #define VEC4F_FMT       "(%f,%f,%f,%f)"
 #define VEC4F_ARG(pvec)  (pvec)->cmp[0], (pvec)->cmp[1], (pvec)->cmp[2], (pvec)->cmp[3]
+
 typedef struct vec2f_t vec2f_t;
 typedef struct vec3f_t vec3f_t;
 typedef struct vec4f_t vec4f_t;
