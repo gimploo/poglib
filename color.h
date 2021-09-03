a64f58d86234c5579e5dd7fae0797eb016d99637
gimploo <gokulkrishnan949@gmail.com>
2021-09-03 15:15:06 +0530

    colors declarations

---
+++ color.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "./math/la.h"
+
+#define BLACK_COLOR     vec3f(0.0f)
+#define RED_COLOR       (vec3f_t ){1.0f, 0.0f, 0.0f}
+#define BLUE_COLOR      (vec3f_t ){0.0f, 0.0f, 1.0f}
+#define GREEN_COLOR     (vec3f_t ){0.0f, 1.0f, 0.0f}
+#define WHITE_COLOR     vec3f(1.0f)
+
