0e7a57fa69b23966f59e0f7c5b004132a8682171
gimploo <gokulkrishnan949@gmail.com>
2022-06-19 23:30:06 +0530

    Update file.h

--- file.h
+++ file.h
@@ -1,115 +1,147 @@
-#ifndef __MY__FILE__H__
-#define __MY__FILE__H__
+#pragma once
+#include "basic.h"
 
 /*===================================================
  // File handling library
 ===================================================*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <assert.h>
-
-#include "basic.h"
-
-typedef struct file_t file_t;
-struct file_t {
+typedef struct file_t {
 
     const char *name;
     FILE        *fp;
+    const char *mode;
     u64         size;
     bool        is_closed;
 
-};
+} file_t ;
 
-/*------------------------------------------
+/*-----------------------------------------------------------------------------
  // Declarations
-------------------------------------------*/
+-------------------------------------------------------------------------------*/
 
-
-file_t          file_init(const char *file_path);
-bool            file_open(file_t *file, const char *mode);
-void            file_read_to_buf(file_t *file, char * const buffer, size_t bufferbytes);
+file_t *        file_init(const char *file_path, const char *mode);
+void            file_readall(const file_t * const file, char *buffer, const u64 buffer_size);
+void            file_writeline(file_t *file, const char *line, const u64 linesize);
+void            file_readline(const file_t *file, char *buffer, u64 buffersize);
 void            file_destroy(file_t * const file);
 
-#define file_close(pfile) {\
-    fclose((*pfile).fp);\
-    (*pfile).is_closed = true;\
-}
-
+#define file(NAME, MODE)\
+    for (file_t *file = file_init(NAME, MODE);\
+            !file->is_closed;\
+            file_destroy(file)) 
 
 /*------------------------------------------
  // Implementataion
 ------------------------------------------*/
 
+u64 file_get_size(const char *filepath)
+{    
+    assert(filepath);
 
-size_t file_get_size(const char *file_path)
-{
-    FILE *fp = fopen(file_path, "r");
-    if (fp == NULL) eprint("%s failed to open", file_path);
-    
+    FILE *fp = fopen(filepath, "r");
+    if (!fp) eprint("file (%s) not found", filepath);
+
     fseek(fp, 0L, SEEK_END);
-    size_t size = ftell(fp);
+    const u64 size = ftell(fp);
     fclose(fp);
-    return size; // Returns the position of the null character 
+    return size;
 }
 
-
-file_t file_init(const char *file_path)
+void __file_set_size(file_t *file)
 {
-    file_t file = {0};
-    file.name = file_path;
-    file.size = file_get_size(file_path);
-    file.fp = NULL;
-    file.is_closed = true;
+    FILE *old_pos = file->fp;
 
-    return file;
+    fseek(file->fp, 0L, SEEK_END);
+    const u64 size = ftell(file->fp);
+    fseek(file->fp, 0L, SEEK_SET);
+
+    file->fp = old_pos;
+    file->size = size;
+
 }
 
-bool file_open(file_t *file, const char *mode)
+bool file_check_exist(const char *filepath)
 {
-    if (file == NULL) return false;
-    file->fp = fopen(file->name, mode);
-    if (file->fp == NULL) return false;
+    assert(filepath);
+    FILE *tmp = fopen(filepath, "r");
+    if (tmp == NULL) return false;
 
-    file->is_closed = false;
+    fclose(tmp);
     return true;
 }
 
 
-void file_read_to_buf(file_t *file,  char * const buffer, size_t bufferbytes)
+file_t * file_init(const char *file_path, const char *mode)
 {
-    assert(file != NULL);
-    assert(buffer != NULL);
-    assert(bufferbytes <= file->size);
+    assert(file_path);
+    assert(mode);
+    file_t file = {0};
+    file.name       = file_path;
+    file.mode       = mode;
+    file.is_closed  = false;
 
-    if (file->is_closed || file->fp == NULL) 
-        if(!file_open(file, "r")) 
-            eprint("Error: unable to open file");
-    
-    fread(buffer, bufferbytes, 1, file->fp);
+    file.fp         = fopen(file_path, mode) ;
+    if (!file.fp) 
+        eprint("unable to open file");
+
+    __file_set_size(&file);
+
+    file_t *tmp = (file_t *)calloc(1, sizeof(file_t ));
+    assert(tmp);
+    *tmp = file;
+
+    return tmp;
 }
 
-void file_readall(file_t * const file, char *buffer, u64 buffer_size)
+void file_readall(const file_t * const file, char *buffer, const u64 buffer_size)
 {
     assert(file);
     assert(buffer);
-    if(buffer_size < file->size) eprint("buffer is too smol, trying to copy %li to %li", file->size, buffer_size);
+    if(buffer_size < file->size) 
+        eprint("buffer is too smol, trying to copy %li to %li", file->size, buffer_size);
 
-    file_read_to_buf(file, buffer, file->size);
-    buffer[file->size] = '\0';
+    fread(buffer, buffer_size, 1, file->fp);
+    buffer[file->size - 1] = '\0';
+}
+
+void file_writeline(file_t *file, const char *line, const u64 linesize)
+{
+    if(file->is_closed) eprint("`%s` file is closed\n", file->name);;
+    assert(line);
+
+    fwrite(line, linesize, 1, file->fp);
+}
+
+
+
+void file_readline(const file_t *file, char *buffer, u64 buffersize)
+{
+    assert(buffersize != 8);
+    assert(!file->is_closed);
+
+    FILE *ptr = file->fp;
+
+    for (u32 i = 0; i < buffersize; i++)
+    {
+        char c = fgetc(ptr);
+        if (c != '\n')
+            buffer[i] = c;
+        else 
+            break;
+    }
 }
 
 void file_destroy(file_t * const file)
 {
     assert(file != NULL);
-    if (!file->is_closed) fclose(file->fp);
 
+    fclose(file->fp);
+    file->mode = NULL;
+    file->size = 0;
     file->fp = NULL;
     file->is_closed = true;
+    free(file);
 }
 
 
 
-#endif // __MY__FILE__H__
