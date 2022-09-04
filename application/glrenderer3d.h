3da628bf65feaea271707dbd790f0872f208f697
gimploo <gokulkrishnan949@gmail.com>
2022-09-04 23:01:28 +0530

    Update glrenderer3d.h

--- application/glrenderer3d.h
+++ application/glrenderer3d.h
@@ -45,36 +45,65 @@ void glrenderer3d_draw_cube(const glrenderer3d_t *renderer)
     assert(renderer->shader);
 
     const f32 CUBE_VERTICES[] = {
-        // front
-        -1.0, -1.0,  1.0,
-         1.0, -1.0,  1.0,
-         1.0,  1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        // back
-        -1.0, -1.0, -1.0,
-         1.0, -1.0, -1.0,
-         1.0,  1.0, -1.0,
-        -1.0,  1.0, -1.0
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     vao_t vao = vao_init();
     vbo_t vbo; 
 
     vao_bind(&vao);
-            const u32 count = MAX_VERTICES_PER_CUBE + MAX_UVS_PER_CUBE;
-            vbo = vbo_static_init(&CUBE_VERTICES, sizeof(CUBE_VERTICES), count);
-            ebo_t ebo = ebo_init(&vbo, DEFAULT_CUBE_INDICES, ARRAY_LEN(DEFAULT_CUBE_INDICES));
-            vao_set_attributes(&vao, &vbo, 3, GL_FLOAT, false, 3 * sizeof(f32), 0);
+            vbo = vbo_init(CUBE_VERTICES);
+            vao_set_attributes(&vao, &vbo, 3, GL_FLOAT, false, 5 * sizeof(f32), 0);
             /*vao_set_attributes(&vao, &vbo, 4, GL_FLOAT, false, sizeof(glvertex_t), offsetof(glvertex_t, color));*/
 
             if (renderer->texture != NULL) {
-                /*vao_set_attributes(&vao, &vbo, 2, GL_FLOAT, false, 2 * sizeof(f32), offsetof(glcube_t, uvs));*/
+                vao_set_attributes(&vao, &vbo, 2, GL_FLOAT, false, 5 * sizeof(f32), 3 * sizeof(f32));
                 /*vao_set_attributes(&vao, &vbo,1, GL_UNSIGNED_INT, false, sizeof(glvertex_t), offsetof(glvertex_t, texture_id));*/
                 gltexture2d_bind(renderer->texture, 0);
             }
 
             glshader_bind((glshader_t *)renderer->shader);
-            vao_draw_with_ebo(&vao, &ebo);
+            vao_draw_with_vbo(&vao, &vbo);
 
     vao_unbind();
 
