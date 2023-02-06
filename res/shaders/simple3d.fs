dd20d5799fbbe2d229a265153ab280698fea198d
gimploo <gokulkrishnan949@gmail.com>
2023-02-06 14:59:25 +0530

    Update simple3d.fs

--- res/shaders/simple3d.fs
+++ res/shaders/simple3d.fs
@@ -3,11 +3,12 @@ out vec4 FragColor;
 
 in vec2 TexCoord;
 
+uniform sampler2D t1;
 
 void main()
 {
     vec3 objectColor = vec3(1.0, 0.5, 0.31);
     vec3 lightColor = vec3(1.0, 1.0, 1.0);
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
-    FragColor = vec4(lightColor * objectColor, 1.0 );
+    FragColor = texture(t1, TexCoord) * vec4(lightColor * objectColor, 1.0 );
 }
