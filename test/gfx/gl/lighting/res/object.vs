5450661cc9b053c4eab73f54fdabc881b3c70f4f
gimploo <gokulkrishnan949@gmail.com>
2023-02-11 18:41:55 +0530

    Lightmap working test

--- test/gfx/gl/lighting/res/object.vs
+++ test/gfx/gl/lighting/res/object.vs
@@ -16,5 +16,5 @@ void main()
     FragPos     = vec3(model * vec4(aPos, 1.0));
     Normal      = mat3(transpose(inverse(model))) * aNormal;
 	TexCoord    = aTexCoord;
-    gl_Position = projection * view * vec4(FragPos, 1.0f);
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }
--- test/gfx/gl/lighting/src/main.c
+++ test/gfx/gl/lighting/src/main.c
@@ -25,8 +25,8 @@ typedef struct TEST {
 void application_init(application_t *app)
 {
     TEST test = (TEST ){
-        /*.shader     = glshader_from_file_init("res/object.vs", "res/lightingMap.fs"),*/
-        .shader     = glshader_from_file_init("res/object.vs", "res/material.fs"),
+        .shader     = glshader_from_file_init("res/object.vs", "res/lightingMap.fs"),
+        /*.shader     = glshader_from_file_init("res/object.vs", "res/material.fs"),*/
         .camera     = glcamera_perspective((vec3f_t ){0.6f, 1.0f, 5.0f}),
         .textures   = {
             gltexture2d_init("res/container2.png"),
@@ -42,20 +42,20 @@ void application_init(application_t *app)
 
     glshader_send_uniform_vec4f(&test.lighting.shader, "color", test.lighting.color);
     
-    /*glshader_send_uniform_lightmap(&test.shader, (gllightmap_t ) {*/
-        /*.label = "material",*/
-        /*.diffuse = 0,*/
-        /*.specular = 1,*/
-        /*.shininess = 64.0f*/
-    /*});*/
-
-    glshader_send_uniform_material(&test.shader, (glmaterial_t ) {
+    glshader_send_uniform_lightmap(&test.shader, (gllightmap_t ) {
         .label = "material",
-        .ambient = (vec3f_t ){1.0f, 0.5f, 0.31f},
-        .diffuse = (vec3f_t ){1.0f, 0.5f, 0.31f},
-        .specular = (vec3f_t ){0.5f, 0.5f, 0.5f},
-        .shininess = 32.0f
+        .diffuse = 0,
+        .specular = 1,
+        .shininess = 64.0f
     });
+
+    /*glshader_send_uniform_material(&test.shader, (glmaterial_t ) {*/
+        /*.label = "material",*/
+        /*.ambient = (vec3f_t ){1.0f, 0.5f, 0.31f},*/
+        /*.diffuse = (vec3f_t ){1.0f, 0.5f, 0.31f},*/
+        /*.specular = (vec3f_t ){0.5f, 0.5f, 0.5f},*/
+        /*.shininess = 32.0f*/
+    /*});*/
 
     glshader_send_uniform_vec3f(&test.shader, "light.ambient", (vec3f_t ){0.2f, 0.2f, 0.2f});
     glshader_send_uniform_vec3f(&test.shader, "light.diffuse", (vec3f_t ){0.5f, 0.5f, 0.5f});
@@ -76,9 +76,9 @@ void application_update(application_t *app)
     glcamera_process_input(&test->camera, application_get_dt(app));
 
     if (window_keyboard_is_key_pressed(win, SDLK_UP))
-        test->lighting.pos.y += 0.2f;
+        test->lighting.pos.z += 0.2f;
     if (window_keyboard_is_key_pressed(win, SDLK_DOWN))
-        test->lighting.pos.y += -0.2f;
+        test->lighting.pos.z += -0.2f;
     if (window_keyboard_is_key_pressed(win, SDLK_RIGHT))
         test->lighting.pos.x += 0.2f;
     if (window_keyboard_is_key_pressed(win, SDLK_LEFT))
@@ -121,6 +121,7 @@ void application_render(application_t *app)
                 /*radians(stopwatch_get_tick() / 150.0f), */
                 /*(vec3f_t ){1.0f, 0.3f, 0.5f});*/
     glshader_send_uniform_matrix4f(&test->shader, "model", model);
+
     glrenderer3d_draw_cube(&(glrenderer3d_t ) {
             .shader = &test->shader,
             .textures = {
