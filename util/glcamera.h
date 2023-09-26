9388c9464a5cba08be3dcd65ae426df5fa5e59ed
gimploo <41667550+gimploo@users.noreply.github.com>
2023-09-26 11:58:09 +0530

    Better manoeuvrability and also removed useless shit

--- util/glcamera.h
+++ util/glcamera.h
@@ -7,24 +7,11 @@
 #define GL_CAMERA_DIRECTION_FRONT    (vec3f_t ){0.0f, 0.0f, -1.0f}
 #define GL_CAMERA_DIRECTION_UP       (vec3f_t ){0.0f, 1.0f, 0.0f }
 
-const f32 GL_CAMERA_YAW             = -90.0f;
-const f32 GL_CAMERA_PITCH           =  0.0f;
-const f32 GL_CAMERA_SPEED           =  2.5f;
-const f32 GL_CAMERA_SENSITIVITY     =  10.0f;
-const f32 GL_CAMERA_ZOOM            =  45.0f;
-const f32 GL_CAMERA_ZOOM_OFFSET     =  2000.0f;
-
-typedef enum glcamera_type {
-
-    GLCAMERATYPE_ORTHOGRAPHIC,
-    GLCAMERATYPE_PERSPECTIVE,
-    GLCAMREATYPE_COUNT
-
-} glcamera_type;
+const f32 GL_CAMERA_SPEED           =  8.0f;
+const f32 GL_CAMERA_SENSITIVITY     =  100.0f;
 
 typedef struct glcamera_t {
 
-    glcamera_type   type;
     vec3f_t         position;
     struct {
         vec3f_t         front;
@@ -32,17 +19,12 @@ typedef struct glcamera_t {
         vec3f_t         right;
         vec3f_t         worldup;
     } direction;
-    struct {
-        f32 yaw;
-        f32 pitch;
-    } angle;
-    f32             zoom;
+    vec2f_t         theta;
     matrix4f_t      __view;
 
 } glcamera_t ;
 
 glcamera_t      glcamera_perspective(const vec3f_t pos);
-glcamera_t      glcamera_orthographic(const vec3f_t pos);
 void            glcamera_process_input(glcamera_t *self, const f32 dt);
 matrix4f_t      glcamera_getview(glcamera_t *self);
 
@@ -56,9 +38,9 @@ void __glcamera_update_directions(glcamera_t *self)
 {
     self->direction.front = glms_normalize(
                                 (vec3f_t ) {
-                                    .x = cos(glm_rad(self->angle.yaw)) * cos(glm_rad(self->angle.pitch)),
-                                    .y = sin(glm_rad(self->angle.pitch)),
-                                    .z = sin(glm_rad(self->angle.yaw)) * cos(glm_rad(self->angle.pitch)),
+                                    .x = cos(self->theta.y) * cos(self->theta.x),
+                                    .y = sin(self->theta.x),
+                                    .z = sin(self->theta.y) * cos(self->theta.x),
                                 });
     // normalize the vectors, because their length gets closer to 0 the more 
     // you look up or down which results in slower movement.
@@ -122,22 +104,21 @@ void glcamera_process_input(glcamera_t *self, const f32 dt)
                             ) 
                         );
 
-    const vec2f_t offset = {
-        .x = (newmp.x - oldmp.x) * GL_CAMERA_SENSITIVITY ,
-        .y = (newmp.y - oldmp.y) * GL_CAMERA_SENSITIVITY  
-    };
-                                
-    self->angle.yaw     += offset.x;
-    self->angle.pitch   += offset.y;
-
-    // TODO: pitch out of bound checks
-
-    if (window_mouse_wheel_is_scroll_up(win))           self->zoom += offset.y;
-    else if (window_mouse_wheel_is_scroll_down(win))    self->zoom -= offset.y;
-
-    //TODO: zoom / fov edge case checks
+    // Looking with the mouse by holding the left mouse button
+    {
+        const f32 nodot = glms_vec2_dot(newmp, oldmp);
+        const f32 mul = glms_vec2_norm(oldmp) * glms_vec2_norm(newmp);
+        const f32 angle = radians(nodot / mul);
+        const vec2f_t direction = glms_vec2_normalize(glms_vec2_sub(newmp, oldmp));
+                                    
+        if (window_mouse_button_is_pressed(win, SDL_MOUSEBUTTON_LEFT)) {
+            self->theta.y += wrap_angle(direction.x * angle * GL_CAMERA_SENSITIVITY * dt);
+            self->theta.x += wrap_angle(direction.y * angle * GL_CAMERA_SENSITIVITY * dt);
+        } 
+    }
 
     __glcamera_update_directions(self);
+
 
     /*printf(VEC3F_FMT"\n", VEC3F_ARG(self->position));*/
     oldmp = newmp;
@@ -146,29 +127,19 @@ void glcamera_process_input(glcamera_t *self, const f32 dt)
 
 matrix4f_t glcamera_getview(glcamera_t *self)
 {
-    switch(self->type)
-    {
-        case GLCAMERATYPE_PERSPECTIVE:
-            self->__view = glms_lookat(
-                            self->position, 
-                            glms_vec3_add(
-                                self->position, 
-                                self->direction.front
-                            ), 
-                            self->direction.up);
-        break;
-
-        case GLCAMERATYPE_ORTHOGRAPHIC:
-        default: eprint("type not accounted for");
-    }
-
+    self->__view = glms_lookat(
+            self->position, 
+            glms_vec3_add(
+                self->position, 
+                self->direction.front
+                ), 
+            self->direction.up);
     return self->__view;
 }
 
 glcamera_t glcamera_perspective(const vec3f_t pos)
 {
     glcamera_t o = {
-        .type       = GLCAMERATYPE_PERSPECTIVE,
         .position   = pos,
         .direction = {
             .front      = GL_CAMERA_DIRECTION_FRONT,
@@ -176,35 +147,11 @@ glcamera_t glcamera_perspective(const vec3f_t pos)
             .right      = glms_normalize(glms_cross(GL_CAMERA_DIRECTION_FRONT, GL_CAMERA_DIRECTION_UP)),
             .worldup    = GL_CAMERA_DIRECTION_UP,
         },
-        .angle = {
-            .yaw    = GL_CAMERA_YAW,
-            .pitch  = GL_CAMERA_PITCH
-        },
-        .zoom       = GL_CAMERA_ZOOM,
+        .theta          = {0},
         .__view       = MATRIX4F_IDENTITY,
     };
-    return o;
-}
 
-glcamera_t glcamera_orthographic(const vec3f_t pos)
-{
-    matrix4f_t projection = {0};
-    glcamera_t o = {
-        .type       = GLCAMERATYPE_ORTHOGRAPHIC,
-        .position   = pos,
-        .direction = {
-            .front      = GL_CAMERA_DIRECTION_FRONT,
-            .up         = glms_normalize(glms_cross(glms_normalize(glms_cross(GL_CAMERA_DIRECTION_FRONT, GL_CAMERA_DIRECTION_UP)), GL_CAMERA_DIRECTION_FRONT)),
-            .right      = glms_normalize(glms_cross(GL_CAMERA_DIRECTION_FRONT, GL_CAMERA_DIRECTION_UP)),
-            .worldup    = GL_CAMERA_DIRECTION_UP,
-        },
-        .angle = {
-            .yaw    = GL_CAMERA_YAW,
-            .pitch  = GL_CAMERA_PITCH
-        },
-        .zoom       = GL_CAMERA_ZOOM,
-        .__view       = MATRIX4F_IDENTITY,
-    };
+    logging("[CAMERA] left click look around and wasd to move the camera\n");
     return o;
 }
 
