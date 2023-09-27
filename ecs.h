94a31cbcf368e37da0bfbf970e094e4c481a6581
gimploo <41667550+gimploo@users.noreply.github.com>
2023-09-27 16:15:49 +0530

    Update ecs.h

--- ecs.h
+++ ecs.h
@@ -1,4 +1,37 @@
 #pragma once
 #include "./ecs/entitymanager.h"
 #include "./ecs/components.h"
-#include "./ecs/systems.h"
+
+//TODO: systems dont work anymore (very old code);
+//#include "./ecs/systems.h"
+
+typedef struct ecs_t {
+
+    entitymanager_t entities;
+    assetmanager_t assets;
+
+    //TODO: Implement systems manager (since entites takes care of components we can have 
+    //a separate manager for systems to make managing easier i think!)
+
+} ecs_t;
+
+
+ecs_t ecs_init(const u32 total_entity_types)
+{
+    return (ecs_t ){
+        .entities = entitymanager_init(total_entity_types),
+        .assets = assetmanager_init() 
+    };
+}
+
+void ecs_update(ecs_t *s, const f32 dt)
+{
+    entitymanager_update(&s->entities);
+}
+
+void ecs_destroy(ecs_t *s)
+{
+    entitymanager_destroy(&s->entities);
+    assetmanager_destroy(&s->assets);
+}
+
