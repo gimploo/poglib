bbcfc08832b3963c55158ed5bf54ec26edaca7b4
gimploo <41667550+gimploo@users.noreply.github.com>
2025-04-27 18:44:03 +0530

    Updated color.h

--- basic/color.h
+++ basic/color.h
@@ -5,6 +5,7 @@
 #define COLOR_BLUE      (vec4f_t ){0.0f, 0.0f, 1.0f, 1.0f}
 #define COLOR_GREEN     (vec4f_t ){0.0f, 1.0f, 0.0f, 1.0f}
 #define COLOR_WHITE     (vec4f_t ){1.0f, 1.0f, 1.0f, 1.0f}
+#define COLOR_NOT_AS_BRIGHT_AS_WHITE     (vec4f_t ){0.9f, 0.9f, 0.9f, 1.0f}
 #define COLOR_CYAN      (vec4f_t ){0.0f, 1.0f, 1.0f, 1.0f}
 #define COLOR_GRAY      (vec4f_t ){0.4f, 0.4f, 0.4f ,1.0f}
 #define COLOR_DARK_GRAY (vec4f_t ){0.1f, 0.1f, 0.1f, 1.0f}
