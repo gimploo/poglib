acfd3b5a8fa28bff9518549c8527e36f6bd008b5
gimploo <41667550+gimploo@users.noreply.github.com>
2025-09-06 17:03:01 +0530

    Fix: Thread safe arenas using mutex (testing required)

--- basic/arena.h
+++ basic/arena.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "dbg.h"
 #include "common.h"
+#include "threads.h"
 
 /*================================================================================
  *                      -- ARENA MEMORY ALLOCATOR --
@@ -22,6 +23,7 @@ struct arena_t {
         u32 count;
     } freelist;
     struct {
+        mtx_t lock;
         struct arena_t *lifetime_owner;
     } meta;
 };
@@ -37,15 +39,17 @@ void        arena_destroy(arena_t *self);
 
 arena_t arena_init(arena_t *arena, u32 capacity)
 {
-    return (arena_t) {
+    arena_t o = {
         .capacity = capacity,
         .size = 0,
         .memory = arena ? arena_reserve(arena, capacity) : calloc(capacity, sizeof(u8)),
         .freelist = {0},
         .meta = {
-            .lifetime_owner = arena
+            .lifetime_owner = arena,
         }
     };
+    mtx_init(&o.meta.lock, mtx_plain);
+    return o;
 }
 
 void * __check_in_freelist(arena_t *self, u32 memory_size)
@@ -60,7 +64,7 @@ void * __check_in_freelist(arena_t *self, u32 memory_size)
                 if (prev_chunk) {
                     prev_chunk->next = chunk->next;
                 } else {
-                    self->freelist.head = chunk;
+                    self->freelist.head = chunk->next;
                 }
                 if(self->meta.lifetime_owner) {
                     arena_giveback(
@@ -83,24 +87,31 @@ void * __check_in_freelist(arena_t *self, u32 memory_size)
 
 void * arena_reserve(arena_t *self, u32 memory_size)
 {
-    if ((self->size + memory_size) > self->capacity)
+    mtx_lock(&self->meta.lock);
     {
-        eprint("Arena is full");
-    }
+        if ((self->size + memory_size) > self->capacity)
+        {
+            eprint("Arena is full");
+        }
 
-    void *res_memory = __check_in_freelist(self, memory_size);
-    if (res_memory) {
-        return res_memory;
-    }
+        void *res_memory = __check_in_freelist(self, memory_size);
+        if (res_memory) {
+            mtx_unlock(&self->meta.lock);
+            return res_memory;
+        }
 
-    void *mem = &self->memory[self->size];
-    self->size += memory_size;
+        void *mem = &self->memory[self->size];
+        self->size += memory_size;
+    }
+    mtx_unlock(&self->meta.lock);
     return mem;
 }
 
 void arena_clear(arena_t *self)
 {
-    self->size = 0;
+    mtx_lock(&self->meta.lock);
+        self->size = 0;
+    mtx_unlock(&self->meta.lock);
 }
 
 void arena_giveback(arena_t *self, void *ptr, u32 size)
@@ -109,43 +120,56 @@ void arena_giveback(arena_t *self, void *ptr, u32 size)
     ASSERT(ptr);
     ASSERT(size > 0);
 
-    free_chunks_t *chunk = self->freelist.head;
-    while(chunk != NULL && chunk->next != NULL) {
-        chunk = chunk->next;
+    mtx_lock(&self->meta.lock);
+    {
+        free_chunks_t *chunk = self->freelist.head;
+        while(chunk != NULL && chunk->next != NULL) {
+            chunk = chunk->next;
+        }
+
+        free_chunks_t *new_chunk = self->meta.lifetime_owner 
+            ? arena_reserve(self->meta.lifetime_owner, sizeof(free_chunks_t))
+            : calloc(1, sizeof(free_chunks_t));
+
+        ASSERT(new_chunk);
+        *new_chunk = (free_chunks_t) {
+            .memory = ptr,
+            .size = size,
+            .next = NULL
+        };
+
+        if (chunk) {
+            chunk->next = new_chunk;
+        } else {
+            self->freelist.head = new_chunk;
+        }
+        self->freelist.count++;
     }
-
-    free_chunks_t *new_chunk = self->meta.lifetime_owner 
-        ? arena_reserve(self->meta.lifetime_owner, sizeof(free_chunks_t))
-        : calloc(1, sizeof(free_chunks_t));
-
-    ASSERT(new_chunk);
-    *new_chunk = (free_chunks_t) {
-        .memory = ptr,
-        .size = size,
-        .next = NULL
-    };
-
-    if (chunk) {
-        chunk->next = new_chunk;
-    } else {
-        chunk = new_chunk;
-    }
-    self->freelist.count++;
+    mtx_unlock(&self->meta.lock);
 }
 
 void arena_destroy(arena_t *self)
 {
     if (self->meta.lifetime_owner) {
         arena_giveback(self->meta.lifetime_owner, self->memory, self->capacity);
+        mtx_destroy(&self->meta.lock);
         return;
     }
-    free_chunks_t *cur = self->freelist.head;
-    while(cur != NULL) {
-        free_chunks_t *chunk = cur;
-        cur = cur->next;
-        free(chunk);
+
+    mtx_lock(&self->meta.lock); 
+    {
+        free_chunks_t *cur = self->freelist.head;
+        while(cur != NULL) {
+            free_chunks_t *chunk = cur;
+            cur = cur->next;
+            free(chunk);
+        }
+        memset(self->memory, 0, self->capacity);
+        free(self->memory);
+
     }
-    memset(self->memory, 0, self->capacity);
-    free(self->memory);
+    mtx_unlock(&self->meta.lock);
+
+    mtx_destroy(&self->meta.lock);
 }
 #endif
