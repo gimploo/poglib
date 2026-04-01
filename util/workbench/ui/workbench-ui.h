a0b4b3d86e435b96bffeb9b54235f07859ac613b
Gokul Krishnan C M <gokulkrishnan949@gmail.com>
2026-04-01 13:44:33 +0530

    bugfix - padding was ignored before

--- util/workbench/ui/workbench-ui.h
+++ util/workbench/ui/workbench-ui.h
@@ -13,6 +13,45 @@ void workbench_compose_ui(const application_t * const app, gui_t *gui)
     const u32 window_width = global_window->width;
     const f32 fps = application_get_fps(app);
 
+        ui_compose_begin(gui, (ui_config_t){ 
+            .composition = {
+                .styles = UI_STYLE_ROUNDED_CORNERS,
+                .traits = UI_BEHAVIOR_HOVERABLE | UI_BEHAVIOR_CLICKABLE
+            },
+            .color = {
+                .base = COLOR_BLUE,
+                .highlight = COLOR_RED,
+            },
+            .dim = {
+                .height = 100,
+                .width = 100 
+            },
+            .padding = {
+                10, 10, 10, 10
+            },
+            .margin = {
+                .left = 10, 
+                .right = 10,
+                .top = 10,
+                .bottom = 10 
+            }
+        });
+            ui_compose_begin(gui, (ui_config_t){ 
+                .composition = {0},
+                .color = {
+                    .base = COLOR_WHITE,
+                    .highlight = COLOR_WHITE,
+                },
+                .dim = {
+                    .height = 40,
+                    .width = 80 
+                },
+                .padding = {0},
+                .margin = {0}
+            });
+            ui_compose_end(gui);
+        ui_compose_end(gui);
+
     for(u8 i = 0; i < 100; i++) {
         ui_compose_begin(gui, (ui_config_t){ 
             .composition = {
