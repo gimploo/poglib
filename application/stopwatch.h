14ed6c19cec02d05815fe10a0da1f7305ae473e6
Gokul Krishnan C M <gokulkrishnan949@gmail.com>
2026-06-30 22:52:50 +0530

    fix: call timeBeginPeriod(1) once at startup instead of in tick function

--- application/stopwatch.h
+++ application/stopwatch.h
@@ -7,6 +7,9 @@
 #include <SDL2/SDL.h>
 #endif
 #include <poglib/basic.h>
+#ifdef _WIN64
+#include <timeapi.h>
+#endif
 
 
 /*=============================================================================
@@ -60,6 +63,9 @@ void stopwatch_delay(const f32 ms)
 
 stopwatch_t stopwatch(void)
 {
+#ifdef _WIN64
+    timeBeginPeriod(1);
+#endif
     stopwatch_t output = {0};
     output.now = stopwatch_get_tick();
     output.raw_dt = 0.01f;
